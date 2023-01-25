b6df89ca2c1aa47597bf560f6a790625484566ce
scv8002 <scv8002@gmail.com>
2023-01-25 15:27:42 +0900

    add zerolog

--- cmd/study-redis-cluster/main.go
+++ cmd/study-redis-cluster/main.go
@@ -1,7 +1,28 @@
 package main
- 
-import "fmt"
- 
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+	"time"
+)
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	zerolog.ErrorFieldName = "reason"
+
+	if true {
+		log.Logger = log.With().Caller().Logger()
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			NoColor:    false,
+			TimeFormat: time.RFC3339,
+		})
+	} else {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	}
+}
+
 func main() {
-    fmt.Println("vim-go")
+	log.Print("hello world")
 }
