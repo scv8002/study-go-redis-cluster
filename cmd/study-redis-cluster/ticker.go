b26abbf11f5c575dc0af77a93d2d4c29bf35d543
scv8002 <scv8002@gmail.com>
2023-02-13 11:45:02 +0900

    ticker test

---
+++ cmd/study-redis-cluster/ticker.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func TestTicker() {
+	fmt.Println("1 goroutine", runtime.NumGoroutine())
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("return ticker loop")
+				return
+			case t := <-ticker.C:
+				fmt.Println("Tick at", t)
+			}
+		}
+	}()
+
+	fmt.Println("2 goroutine", runtime.NumGoroutine())
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+	done <- true
+
+	fmt.Println("3 goroutine", runtime.NumGoroutine())
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("4 goroutine", runtime.NumGoroutine())
+}
